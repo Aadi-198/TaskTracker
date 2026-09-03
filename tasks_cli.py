381a2b51fcee4710ecfc7e0a51b6370e0c4d0fa7
Pro <aadishresth@gmail.com>
2026-09-03 12:05:36 +0530

    make functions for all the commands

--- tasks_cli.py
+++ tasks_cli.py
@@ -4,7 +4,7 @@ import json
 from datetime import datetime
 
 from db import tasks_file, load_tasks, check_ID, task_exists
-from tasks_manager import add_tasks
+from tasks_manager import add_tasks, list_tasks, delete_tasks, update_tasks
 
 #print("Arguments passed:", sys.argv)
 
@@ -15,34 +15,11 @@ if len(sys.argv) > 1:
     if command == "add":
         add_tasks()
     elif command == "list":
-        task_list = load_tasks()
-        for task in task_list:
-            print(f"[{task['id']}] {task['description']} ({task['status']})")
+        list_tasks()
     elif command == "delete":
-            task_id = check_ID()
-            if not task_exists(task_id):
-                sys.exit()  
-            else:
-                task_list = load_tasks()
-                task_list = [t for t in task_list if t["id"] != task_id]
-                tasks_file.write_text(json.dumps(task_list, indent = 4))
-                print(f"Successfully deleted task with ID: {task_id}")
+            delete_tasks()
     elif command == "update":
-        task_id = check_ID()
-        if not task_exists(task_id):
-            sys.exit()  
-        else:
-            if len(sys.argv) > 3:
-                new_description = sys.argv[3]
-                task_list = load_tasks()
-                for task in task_list:
-                    if task["id"] == task_id:
-                        task["createdAt"] = datetime.now().isoformat()
-                        task["description"] = new_description
-                        tasks_file.write_text(json.dumps(task_list, indent = 4))
-                        print(f"Successfully updated task {task_id} to {new_description}")
-            else:
-                print("Argument required: new description")
+        update_tasks()
     else:
         print(f"{command}: command not found")
 else:
--- tasks_manager.py
+++ tasks_manager.py
@@ -2,7 +2,7 @@ import sys
 import json
 from datetime import datetime
 
-from db import tasks_file, load_tasks
+from db import tasks_file, load_tasks, check_ID, task_exists
 
 def add_tasks():
     if len(sys.argv) > 2:
@@ -22,3 +22,35 @@ def add_tasks():
         print(f"Successfully added task(s): {task}")
     else:
         print("Argument required: tasks")
+
+def list_tasks():
+    task_list = load_tasks()
+    for task in task_list:
+        print(f"[{task['id']}] {task['description']} ({task['status']})")
+
+def delete_tasks():
+    task_id = check_ID()
+    if not task_exists(task_id):
+        sys.exit()  
+    else:
+        task_list = load_tasks()
+        task_list = [t for t in task_list if t["id"] != task_id]
+        tasks_file.write_text(json.dumps(task_list, indent = 4))
+        print(f"Successfully deleted task with ID: {task_id}")
+
+def update_tasks():
+    task_id = check_ID()
+    if not task_exists(task_id):
+        sys.exit()  
+    else:
+        if len(sys.argv) > 3:
+            new_description = sys.argv[3]
+            task_list = load_tasks()
+            for task in task_list:
+                if task["id"] == task_id:
+                    task["createdAt"] = datetime.now().isoformat()
+                    task["description"] = new_description
+                    tasks_file.write_text(json.dumps(task_list, indent = 4))
+                    print(f"Successfully updated task {task_id} to {new_description}")
+        else:
+            print("Argument required: new description")
