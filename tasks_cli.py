1054bd08f00fa73e1327264bae23a6c454cc306f
Pro <aadishresth@gmail.com>
2026-09-03 12:11:56 +0530

    remove unused imports

--- tasks_cli.py
+++ tasks_cli.py
@@ -1,17 +1,11 @@
 #import required libraries
 import sys
-import json
-from datetime import datetime
 
-from db import tasks_file, load_tasks, check_ID, task_exists
 from tasks_manager import add_tasks, list_tasks, delete_tasks, update_tasks
 
-#print("Arguments passed:", sys.argv)
-
-# check if the typed script has an command and an input
 if len(sys.argv) > 1:
     command = sys.argv[1]   #if script has a command, save it in a var called command
-    # define what commands do
+    # call commands defined in task_manager.py
     if command == "add":
         add_tasks()
     elif command == "list":
